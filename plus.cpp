634426b1ac8c15190a5432fba86d15b58030444a
OLOFMEISTER <86936004+ZhongYN007@users.noreply.github.com>
2022-10-10 20:00:10 +0800

    Add files via upload

---
+++ plus.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    char b;
+    float c;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    float d = (float)a+(float)b+(float)c;
+    printf("%.2f",d);
+}
